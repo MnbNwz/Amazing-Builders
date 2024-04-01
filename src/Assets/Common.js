92089d338f9dbff108c2c8383382eb2a9a046f50
Muneeb Nawaz <muneebnawaz2018@gmail.com>
2024-04-01 14:32:16 +0500

    Common added

--- src/Assets/Common.js
+++ src/Assets/Common.js
@@ -15,7 +15,40 @@ let Common = {
         architectureModelingText: "A good home design considers all aspects of the site and building – orientation, site features, thermal performance, floor plan, materials, finishes, technologies, appliances, and landscape. Using a qualified architect we ensure good design, better quality outcomes and a smoother construction process.",
         contructionManagement: "Contruction Management",
         contructionManagementText: "It is essential to effectively oversee on-site operations, such as personnel and materials to ensure quality builds. We plan, coordinate, budget, and supervise the project from start to finish.",
-    }
+    },
+    aboutUs: "About Us",
+    aboutUsText1: "Founded by two partners who had different skills and experiences but a shared vision and entrepreneurial spirit, Matrix Designer Builders is a rising Australian home builder with a wide array of experience in residential housing.",
+    aboutUsText2: "In an industry being filled with companies that simply reproduce more and more variants of the same house – as if each fell from a giant cookie-cutter. Few bother to challenge the status quo or alter the norm. Building a home should be an exciting and personal journey and the finished product a symbol of pride. This is why Matrix specialise in custom homes guiding clients through this personal journey.",
+    aboutUsText3: "In partnership with our valued trades and suppliers, we pride ourselves on delivering quality homes in communities where our customers want to live.",
+    recentProjects: "RECENT PROJECTS",
+    recentProjectsArray: [
+        "Project 1 - Hartland 25",
+        "Project 2 - Sandygate 27",
+        "Project 3 - Restful 22",
+        "Project 4 - Grazing"
+    ],
+    partners: "PARTNERS",
+    contact: "CONTACT",
+    inquiry: "Inquiries",
+    inquiryText: "For any inquiries, questions or commendations, please call: 0418 708 559 or fill out the following form",
+    headOffice: "Head Office",
+    headOfficeAddress: `53 Ravenhall Way
+    Ravenhall, VIC 3037`,
+    headOfficeMail: "info@matrixdesignerbuilders.com.au",
+    headOfficeTelephone: "0418 708 559",
+    employement: "Employment",
+    exmpoymenetText: "To apply for a job with Matrix Designer Builds, please send a cover letter together with your C.V. to: info@matrixdesignerbuilders.com.au",
+    getA_Qoute: "Get a quote: 0418-708-559",
+    socialMedia: "Socials/Media",
+    copywriteText: "by Matrix Designer Builders.",
+    contactUs: "Contact Us",
+    firstName: "First Name",
+    lastName: "Last Name",
+    email: "Email",
+    subject: "Subject",
+    message: "Message",
+    submit: "Submit"
+
 }
 
 export default Common;
