16727df8fdb19383426646dd7f0fd22324fe3365
Muneeb Nawaz <muneebnawaz2018@gmail.com>
2024-04-03 15:50:08 +0500

    about asset added

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./AboutUs.css";
+import ComponentHeader from "./../ComponentHeader/ComponentHeader";
+import Common from "../../Common";
 
 const About_Us = () => {
-  return <div>About_Us</div>;
+  return (
+    <div className="about-section">
+      <div className="about-content">
+        <div className="col-6">
+          <div className="row">
+            <ComponentHeader text={Common.aboutUs} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About_Us;
