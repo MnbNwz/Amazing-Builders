b8711133cdbaf08b55ea9b46daf580ecc596fbdc
Muneeb Nawaz <muneebnawaz2018@gmail.com>
2024-04-03 14:35:58 +0500

    Intro done

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Intro.css";
+import Common from "../../Common";
 
-const Intro = () => {
-  return <div>Intro</div>;
-};
+function YourComponent() {
+  return (
+    <div className="col-12 center-content">
+      <div className="col-6">
+        <div className="row">
+          <h1 className="heading1">{Common.carouselText}</h1>
+        </div>
+        <div className="row" style={{ justifyContent: "center" }}>
+          <div className="down-arrow" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-export default Intro;
+export default YourComponent;
