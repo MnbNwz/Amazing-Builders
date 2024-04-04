b65f18bf901de81c5adf12f2a33e1c8b0427adec
Muneeb Nawaz <muneebnawaz2018@gmail.com>
2024-04-04 12:42:21 +0500

    About Us image card implemented

--- src/Components/RecentProjects/RecentProjects.jsx
+++ src/Components/RecentProjects/RecentProjects.jsx
@@ -2,12 +2,41 @@ import React from "react";
 import ProjectsCount from "./ProjectsCount";
 import ComponentHeader from "../ComponentHeader/ComponentHeader";
 import Common from "../../Common";
+import hartLand from "./../../Assets/hartLand_Img.jpg";
+import grazing from "./../../Assets/grazing_Img.jpg";
+import restFull from "./../../Assets/restFull_Img.jpg";
+import sandyGate from "./../../Assets/sandyGate_Img.jpg";
+import ProjectImage from "./ProjectImage";
 
 const Recent_Projects = () => {
   return (
     <>
       <ProjectsCount />
       <ComponentHeader text={Common.recentProjects} />
+      <div className="col-12 project-container">
+        <div className="col-6">
+          <div className="row">
+            <div className="project-image-container">
+              <ProjectImage
+                src={hartLand}
+                alt="Image 1"
+                overlayText="Image 1"
+              />
+              <ProjectImage src={grazing} alt="Image 2" overlayText="Image 2" />
+              <ProjectImage
+                src={restFull}
+                alt="Image 1"
+                overlayText="Image 1"
+              />
+              <ProjectImage
+                src={sandyGate}
+                alt="Image 2"
+                overlayText="Image 2"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
